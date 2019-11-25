0b0399980bc2f46e91fa248968056c40aa4547a6
Nguyen <hai.b.nguyen@accenturefederal.com>
2019-11-25 10:24:43 -0600

    minor

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -643,6 +643,163 @@ export class DataService {
       "server_app_cod_https_url": "https://web-tester.com",
       "server_svc_ws_https_url": "https://web-tester.com"
     }
+  },
+  {
+    "env": {
+      "environment_type": "env1",
+      "oracle_connection": {
+        "cod_tds": {
+          "primary": {
+            "node1": {
+              "host": "test.amazonaws.com",
+              "service": "txn",
+              "port": "1000",
+              "ip": ""
+            }
+          }
+        },
+        "cod_rds": {
+          "primary": {
+            "node1": {
+              "host": "test.amazonaws.com",
+              "ip": "1.1.1.1",
+              "port": "1000",
+              "service": "rpt"
+            }
+          }
+        }
+      },
+      "webserver_host": "1.1.2.3",
+      "api_port": "12345",
+      "sql_host": "1.1.2.3",
+      "props": {
+        "cod": {
+          "config": {
+            "di_node": {
+              "mq_host_2": "foo.bar.baz.quux",
+              "mq_host_1": "foo.bar.baz.quux",
+              "mq_port_2": "1234",
+              "mq_port_1": "1234"
+            }
+          }
+        }
+      },
+      "schemaprefix": {
+        "sl": "cod",
+        "cod": "cod",
+        "ats": "ats"
+      },
+      "schemas": {
+        "oracle": {
+          "sl": "SL{{environment_name|upper}}",
+          "cod": "COD{{environment_name|upper}}"
+        }
+      },
+      "queue_manager": "CODT1",
+      "host_connections": {
+        "env1-message1_ip": "1.1.1.1",
+        "env1-message1_port": 12345,
+        "env1-svcapp1_ip": "1.1.1.1",
+        "env1-svcapp1_port": 12345,
+        "env1-rptwebedw1_ip": "1.1.1.1",
+        "env1-rptwebedw1_port": 12345,
+        "env1-batch2_ip": "1.1.1.1",
+        "env1-batch2_port": 12345,
+        "env1-batch1_ip": "1.1.1.1",
+        "env1-batch1_port": 12345,
+        "env1-rptappcod1_ip": "1.1.1.1",
+        "env1-rptappcod1_port": 12345,
+        "env1-web2_ip": "1.1.1.1",
+        "env1-web2_port": 12345,
+        "env1-cache2_ip": "1.1.1.1",
+        "env1-cache2_port": 12345,
+        "env1-svcbatch2_ip": "1.1.1.1",
+        "env1-svcbatch2_port": 12345,
+        "env1-rptappcod2_ip": "1.1.1.1",
+        "env1-rptappcod2_port": 12345,
+        "env1-web1_ip": "1.1.1.1",
+        "env1-web1_port": 12345,
+        "env1-cache1_ip": "1.1.1.1",
+        "env1-cache1_port": 12345,
+        "env1-rptappedw1_ip": "1.1.1.1",
+        "env1-rptappedw1_port": 12345,
+        "env1-svcapp2_ip": "1.1.1.1",
+        "env1-svcapp2_port": 12345,
+        "env1-svcdoc1_ip": "1.1.1.1",
+        "env1-svcdoc1_port": 12345,
+        "env1-stubs_ip": "1.1.1.1",
+        "env1-stubs_port": 12345,
+        "env1-app2_ip": "1.1.1.1",
+        "env1-app2_port": 12345,
+        "env1-rptwebcod1_ip": "1.1.1.1",
+        "env1-rptwebcod1_port": 12345,
+        "env1-rptldapedw1_ip": "1.1.1.1",
+        "env1-rptldapedw1_port": 12345,
+        "env1-dbclient1_ip": "1.1.1.1",
+        "env1-dbclient1_port": 12345,
+        "env1-rptappedw2_ip": "1.1.1.1",
+        "env1-rptappedw2_port": 12345,
+        "env1-cms1_ip": "1.1.1.1",
+        "env1-cms1_port": 12345,
+        "env1-rptldapcod1_ip": "1.1.1.1",
+        "env1-rptldapcod1_port": 12345,
+        "env1-schedule1_ip": "1.1.1.1",
+        "env1-schedule1_port": 12345,
+        "env1-svcbatch1_ip": "1.1.1.1",
+        "env1-svcbatch1_port": 12345,
+        "env1-svcdoc2_ip": "1.1.1.1",
+        "env1-svcdoc2_port": 12345,
+        "env1-app1_ip": "1.1.1.1",
+        "env1-app1_port": 12345
+      },
+      "server_rpt_cog_https_ip": "1.1.1.1",
+      "server_rpt_cog_https_port": 12345,
+      "server_app_sl_https_ip": "1.1.1.1",
+      "server_app_sl_https_port": 12345,
+      "server_app_sl_cluster_ip": "1.1.1.1",
+      "server_app_sl_cluster_port": 12345,
+      "server_svc_ats_https_ip": "1.1.1.1",
+      "server_svc_ats_https_port": 12345,
+      "server_svc_ctrl_https_ip": "1.1.1.1",
+      "server_svc_ctrl_https_port": 12345,
+      "server_svc_crw_cluster_ip": "1.1.1.1",
+      "server_svc_crw_cluster_port": 12345,
+      "server_app_ats_https_ip": "1.1.1.1",
+      "server_app_ats_https_port": 12345,
+      "server_svc_ats_cluster_ip": "1.1.1.1",
+      "server_svc_ats_cluster_port": 12345,
+      "server_svc_crw_https_ip": "1.1.1.1",
+      "server_svc_crw_https_port": 12345,
+      "server_svc_cc_cluster_ip": "1.1.1.1",
+      "server_svc_cc_cluster_port": 12345,
+      "server_app_adm_http_ip": "1.1.1.1",
+      "server_app_adm_http_port": 12345,
+      "server_app_cod_cluster_ip": "1.1.1.1",
+      "server_app_cod_cluster_port": 12345,
+      "server_app_adm_https_ip": "1.1.1.1",
+      "server_app_adm_https_port": 12345,
+      "server_app_ats_cluster_ip": "1.1.1.1",
+      "server_app_ats_cluster_port": 12345,
+      "server_svc_ctrl_cluster_ip": "1.1.1.1",
+      "server_svc_ctrl_cluster_port": 12345,
+      "server_svc_crw_http_ip": "1.1.1.1",
+      "server_svc_crw_http_port": 12345,
+      "server_svc_ws_cluster_ip": "1.1.1.1",
+      "server_svc_ws_cluster_port": 12345,
+      "server_svc_ws_https_ip": "1.1.1.1",
+      "server_svc_ws_https_port": 12345,
+      "server_app_cod_https_ip": "1.1.1.1",
+      "server_app_cod_https_port": 12345,
+      "server_svc_cc_https_ip": "1.1.1.1",
+      "server_svc_cc_https_port": 12345,
+      "server_svc_cc_https_url": "https://test-test.com",
+      "server_console_ip": "1.1.1.1",
+      "server_console_port": 12345,
+      "server_console_url": "https://test-test.com",
+      "server_svc_ws_https_url": "https://test-test.com",
+      "server_app_sl_https_url": "https://test-test.com",
+      "server_app_cod_https_url": "https://test-test.com"
+    }
   }
   ];
 }
